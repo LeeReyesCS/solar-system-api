c845ddac955545e6a189e2e0fe4973258622f861
Michelle Oh <ohmotrl@gmail.com>
2022-11-01 10:44:25 -0400

    added get, put, and delete methods

--- app/routes.py
+++ app/routes.py
@@ -29,25 +29,6 @@ def handle_planets():
         db.session.commit()
     
     return make_response(f"Planet {new_planet.name} successfully create", 201)
-    
-# class Planet:
-#     def __init__(self, id, name, description, num_moons):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.num_moons = num_moons
-
-# PLANETS = [
-#     Planet(1, "Mercury", "Closest to the Sun", 0),
-#     Planet(2, "Venus", "The Planet of Love", 0),
-#     Planet(3, "Earth", "The Planet of Life", 1),
-#     Planet(4, "Mars", "The Red Planet", 2),
-#     Planet(5, "Jupiter", "A gas giant planet", 57),
-#     Planet(6, "Saturn", "The Planet with Rings",63),
-#     Planet(7, "Uranus", "It's really cold here",27),
-#     Planet(8, "Neptune", "Dark, Cold, Ice Giant",14)
-#     ]
-
 
 # @planet_bp.route("", methods=["GET"])
 # def handle_planets():
@@ -89,7 +70,7 @@ def update_one_planet(id):
     
     db.session.commit()
     
-    return make_response(f"Planet #{id} successfully updated")
+    return make_response(f"Planet #{id} successfully updated", 200)
 
 ## delete planet
 @planet_bp.route("/<id>", methods=["DELETE"])
@@ -99,4 +80,34 @@ def delete_planet(id):
     db.session.delete(planet)
     db.session.commit()
     
-    return make_response(f"Planet #{id} successfully deleted")
+    return make_response(f"Planet #{id} successfully deleted", 202)
+
+@planet_bp.route("/<id>", methods=["GET", "PUT", "DELETE"])
+def handle_planet(id):
+    planet = Planet.query.get(id)
+    
+    if request.method == "GET":
+        return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "num_moons": planet.num_moons
+        }
+    elif request.method == "PUT":
+        request_body = request.get_json()
+        
+        planet.name = request_body["name"]
+        planet.description = request_body["description"] 
+        planet.num_moons = request_body["num_moons"] 
+        
+        db.session.commit()
+        
+        return make_response(f"Planet #{id} successfully updated", 200)
+    
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return make_response(f"Planet #{id} successfully deleted", 202)    
+            
+            
+        
